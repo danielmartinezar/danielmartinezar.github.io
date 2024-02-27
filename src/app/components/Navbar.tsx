5a96289febcbe36df29f10ca117677344e166318
danielmartinezar <danielma1205@hotmail.com>
2024-02-27 17:15:31 -0500

    Navbar changed

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,22 +6,22 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
 export default function Navbar() {
   const [navbarOpen, setNavbarOpen] = useState(false);
-  const [isSmallScreen, setIsSmallScreen] = useState(
-    window.matchMedia("(max-width: 1024px)").matches
-  );
+  // const [isSmallScreen, setIsSmallScreen] = useState(
+  //   window.matchMedia("(max-width: 1024px)").matches
+  // );
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.matchMedia("(max-width: 1024px)").matches);
-    };
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setIsSmallScreen(window.matchMedia("(max-width: 1024px)").matches);
+  //   };
 
-    handleResize();
-    window.addEventListener("resize", handleResize);
+  //   handleResize();
+  //   window.addEventListener("resize", handleResize);
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
@@ -56,9 +56,7 @@ export default function Navbar() {
           </ul>
         </div>
       </div>
-      {navbarOpen && isSmallScreen ? (
-        <MenuOverlay />
-      ) : null}
+      {navbarOpen ? <MenuOverlay /> : null}
     </nav>
   );
 }
